eff32a79b35cbac7778658f64cc0aba0266e46b1
Sherief Farid <sherief@farid.cloud>
2019-03-14 00:05:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,19 @@
 
 def key_for_min_value(name_hash)
 
+  values = []
+
+  name_hash.collect do |key, value|
+    values << value
+  end
+
+  values.sort
+  return_value = values[0]
+
+  name_hash.each do |key,value|
+    if value == return_value
+    return key
+  end
+
+  
 end
