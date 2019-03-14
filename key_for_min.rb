032b0ad18c424132f4c2ed19ed8d697f391633e6
Sherief Farid <sherief@farid.cloud>
2019-03-14 00:11:15 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,19 @@
 
 def key_for_min_value(name_hash)
 
+  values = []
+  return_key = nil
+  name_hash.collect do |key, value|
+    values << value
+  end
+
+  sorted_values = values.sort
+  return_value = sorted_values[0]
+
+  name_hash.each do |key,value|
+    if value == return_value
+    return_key = key
+    end
+  end
+return return_key
 end
